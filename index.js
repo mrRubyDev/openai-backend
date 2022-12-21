e69c6d7b90fdf21b705bd9bc6c5935460591a511
Mateo <mateorubinstein@icloud.com>
2022-12-21 22:42:33 +0100

    Enable cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const openAiRoutes = require("./routes/openai.routes");
 const port = process.env.PORT || 3001;
@@ -8,6 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 app.use("/openai", openAiRoutes);
 
