5ebe96e4981e3ad8b39120dce2fa3771a02c30b0
Mateo <mateorubinstein@icloud.com>
2022-12-21 22:31:37 +0100

    Change get for post

--- routes/openai.routes.js
+++ routes/openai.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { generateTweet } = require("../controllers/openai.controller.js");
 const router = express.Router();
 
-router.get("/twitter", generateTweet);
+router.post("/twitter", generateTweet);
 
 module.exports = router;
