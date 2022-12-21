2fa1929ee56f3120a80c5a9036fccb0a55509e6b
Mateo <mateorubinstein@icloud.com>
2022-12-21 22:12:42 +0100

    Uninstall body-parser

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const bodyParser = require("body-parser");
 
 const openAiRoutes = require("./routes/openai.routes");
 const port = process.env.PORT || 3001;
@@ -8,8 +7,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/openai", openAiRoutes);
 
