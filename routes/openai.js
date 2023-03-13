8d071f42cc98233f2d8ba0be8265985194b1a3e6
Mateo <mateorubinstein@icloud.com>
2023-03-13 10:11:57 +0100

    Improve prompt

--- routes/openai.js
+++ routes/openai.js
@@ -14,7 +14,8 @@ router.post("/text", async (req, res) => {
       model: "text-davinci-003",
       prompt:
         text +
-        `I need help repurposing my newsletter (the previous text) for a twitter thread, but I need it done in the following format: #Headline, #Short sentence and 4 bullet points. Make it into 3 tweets, and return them in a json format.`,
+        "Repurpose the previous text into a twitter thread. The thread should be 3 tweets. They all should include bullet points, and a couple sentences that summarize them.",
+      // `I need help repurposing my newsletter (the previous text) for a twitter thread, but I need it done in the following format: ·Tweet Headline\n ·Tweet Short sentence\n ·Bullet point\n ·Bullet point\n ·Bullet point\n ·Bullet point\n. Avoid using hashtags. Don't return "headline", "short sentence" or "bullet points". Make it into 3 tweets.`,
       max_tokens: 2048,
       temperature: 0.5,
       frequency_penalty: 0.5,
